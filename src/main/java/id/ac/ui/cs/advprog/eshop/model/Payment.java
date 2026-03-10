61974c449b23c0f08a4aa23e7ab56c2130d521b8
RousanChandra <syahrousan@gmail.com>
2026-03-10 20:30:44 +0700

    [REFACTOR] Enhance PaymentRepository with Collections and Stream API

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,11 +1,15 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.Map;
 
-@Builder @Getter @Setter
+@Getter @Setter @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
     private String id;
     private String method;
